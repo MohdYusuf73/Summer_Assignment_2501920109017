3bca5f4e13690b2a858cde84f49a9da55beea818
MohdYusuf73 <yusufshaikh9873@gmail.com>
2026-06-09 18:41:26 +0530

    Week 2 Day 2 assignment

---
+++ Week_2/Day_2/findAllAnagram.java
@@ -0,0 +1,32 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        List<Integer> ans = new ArrayList<>();
+
+        if (p.length() > s.length()) {
+            return ans;
+        }
+
+        int[] pCount = new int[26];
+        int[] winCount = new int[26];
+
+        for (int i = 0; i < p.length(); i++) {
+            pCount[p.charAt(i) - 'a']++;
+            winCount[s.charAt(i) - 'a']++;
+        }
+
+        if (Arrays.equals(pCount, winCount)) {
+            ans.add(0);
+        }
+
+        for (int i = p.length(); i < s.length(); i++) {
+            winCount[s.charAt(i) - 'a']++;
+            winCount[s.charAt(i - p.length()) - 'a']--;
+
+            if (Arrays.equals(pCount, winCount)) {
+                ans.add(i - p.length() + 1);
+            }
+        }
+
+        return ans;
+    }
+}
